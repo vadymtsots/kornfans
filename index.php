feb48d394be6b81ef55ce8b347782fb6efa62c50
tsots <vadymtsots@gmail.com>
2021-09-24 21:15:33 +0300

    Basic fronend in a single file

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/styles/styles.css">
+    <title>Kornfans</title>
+</head>
+<body>
+
+<header class="header">
+    <a class="logo" href="/">
+        <h1>KORNFANS</h1>
+    </a>
+</header>
+
+<aside>
+    <ul>
+        <li><a href="#">History</a></li>
+        <li><a href="#">Discography</a></li>
+        <li><a href="#">Members</a></li>
+        <li><a href="#">Reviews</a></li>
+    </ul>
+</aside>
+
+<section class="content">
+    <p>
+        Korn (stylized as KoЯn) is an American nu metal band from Bakersfield, California, formed in 1993.
+        The band is notable for pioneering the nu metal genre and bringing it into the mainstream.
+    </p>
+    <p>
+        Originally formed in 1993 by three members of the band L.A.P.D., Korn's current lineup features founding members James "Munky" Shaffer (rhythm guitar),
+        Reginald "Fieldy" Arvizu (bass), Brian "Head" Welch (lead guitar, backing vocals), and Jonathan Davis (lead vocals, bagpipes),
+        with the addition of Ray Luzier (drums) in 2007, replacing the band's first drummer, David Silveria.
+    </p>
+</section>
+
+<footer>
+    <p>Created by Tsots</p>
+</footer>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
